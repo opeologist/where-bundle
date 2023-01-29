38314b37e7219d1edbc7df2a37c534fcf94c202d
Aaron Giordano-Barry <opeologist@gmail.com>
2023-01-29 05:55:08 -0500

    file/folder adjustments

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,10 @@
-import { html, createRoot, React } from "./deps.mjs";
+import { html, createRoot } from "./deps.mjs";
 import { Header } from "./components/Header.mjs";
 
-const { StrictMode } = React;
 let App = () =>
-  html`<${StrictMode}>
-    <main>
-      <${Header} />
-    </main>
-  <//>`;
+  html`<main>
+    <${Header} />
+  </main>`;
 
 createRoot(document.getElementById("root")).render(html`<${App} />`);
 
--- src/index2.mjs
+++ src/index2.mjs
@@ -1,7 +1,6 @@
-import { html, createRoot, React } from "./deps.mjs";
+import { html, createRoot } from "./deps.mjs";
 
-const { StrictMode } = React;
-let App = () => html`<${StrictMode}><h1>world</h1><//>`;
+let App = () => html`<h1>world</h1>`;
 
 createRoot(document.getElementById("root2")).render(html`<${App} />`);
 
