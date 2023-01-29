bb3c78c3e9a6bd42147fad80690fa84a958d1969
Aaron Giordano-Barry <opeologist@gmail.com>
2023-01-29 06:16:11 -0500

    add react-router

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { html, createRoot } from "./deps.mjs";
 import { Header } from "./components/Header.mjs";
 
-let App = () =>
-  html`<main>
-    <${Header} />
-  </main>`;
+let App = () => html`<main>
+  <${Header} />
+</main>`;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: html`<${App} />`,
+  },
+]);
 
-createRoot(document.getElementById("root")).render(html`<${App} />`);
+createRoot(document.getElementById("root")).render(
+  html`<${RouterProvider} router=${router} />`
+);
 
 if (import.meta.hot) {
   import.meta.hot.accept((module) => {
